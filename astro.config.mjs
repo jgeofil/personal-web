5440d631df923a49abbcf5ad47acb12b275fab01
Jeremy Georges-Filteau <jeremy.geo@gmail.com>
2026-02-22 19:20:15 -0500

    feat: Integrate Statsig for web analytics, adopt Bun for package management, and configure Astro content collection types.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,9 @@
 import { defineConfig } from 'astro/config';
-// Use Vercel Edge Functions (Recommended)
-import vercel from '@astrojs/vercel/edge';
-// Can also use Serverless Functions
-// import vercel from '@astrojs/vercel/serverless';
-// Or a completely static build
-// import vercel from '@astrojs/vercel/static';
+// Use Vercel adapter
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   output: 'server',
-  experimental: {
-    assets: true
-   },
   adapter: vercel({
     imageService: true,
     webAnalytics: { enabled: true }
